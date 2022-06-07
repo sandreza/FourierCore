185182b9d34a816091e3839732dd0114746b00c5
Andre Souza <andrenogueirasouza@gmail.com>
2022-06-07 14:43:47 -0400

    saving output

--- examples/kuramoto_sivashinsky.jl
+++ examples/kuramoto_sivashinsky.jl
@@ -1,5 +1,5 @@
 using FourierCore, FourierCore.Grid, FourierCore.Domain
-using FFTW, LinearAlgebra, BenchmarkTools, Random, JLD2
+using FFTW, LinearAlgebra, BenchmarkTools, Random, JLD2, HDF5
 using GLMakie
 # using CUDA
 
@@ -9,7 +9,7 @@ Random.seed!(123456789)
 arraytype = Array
 L = 22 # 20π * sqrt(2) # 22
 Ω = S¹(L)
-N = 2^8 # number of gridpoints
+N = 2^5 * 8  # number of gridpoints
 grid = FourierGrid(N, Ω, arraytype=arraytype)
 nodes, wavenumbers = grid.nodes, grid.wavenumbers
 
@@ -54,12 +54,12 @@ function nonlinear_term!(u²x, uⁿ, u², P!, P⁻¹!, ∂x, filter)
     return nothing
 end
 
-@. uⁿ = sin(2π / L * x) + 0.1 * cos(2 * 2π/L * x) + 0.1 * sin(11 * 2π / L * x)
+@. uⁿ = sin(2π / L * x) + 0.1 * cos(2 * 2π / L * x) + 0.1 * sin(11 * 2π / L * x)
 P! * uⁿ
 
 substep = 30
 endtime = 1000
-M = floor(Int, endtime / (30 * Δt))
+M = floor(Int, endtime / (substep * Δt))
 plotmat = zeros(N, M)
 
 tic = Base.time()
@@ -74,14 +74,27 @@ for i in 1:M
     plotmat[:, i] .= real.(uⁿ)
     P! * uⁿ
 end
-toc = Base.time() 
+toc = Base.time()
 println("The time for the simulation was ", toc - tic, " seconds")
 
-fig = Figure(resolution = (1000, 500))
-ax = Axis(fig[1,1])
+fig = Figure(resolution=(1000, 500))
+ax = Axis(fig[1, 1])
 ax_spec = Axis(fig[1, 2])
-ax_inst = Axis(fig[1,3])
+ax_inst = Axis(fig[1, 3])
 contourf!(ax, plotmat)
 scatter!(ax_spec, log.(abs.(uⁿ) .+ eps(1.0)))
-lines!(ax_inst, plotmat[:,end])
+lines!(ax_inst, plotmat[:, end])
 display(fig)
+
+
+if N == 32
+    fid = h5open("low_res.h5", "w")
+    println("saving low rez")
+    fid["u"] = plotmat
+    close(fid)
+elseif N == 256
+    fid = h5open("high_res.h5", "w")
+    println("saving high rez")
+    fid["u"] = plotmat
+    close(fid)
+end

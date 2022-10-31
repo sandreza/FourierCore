f719f069338a46ad205025cab62095ef32302abe
sandreza <andrenogueirasouza@gmail.com>
2022-10-31 13:12:14 -0400

    big calculation

--- examples/ensemble_evolution_time_dependent.jl
+++ examples/ensemble_evolution_time_dependent.jl
@@ -143,163 +143,155 @@ r_A = Array(@. sqrt((x - 2π)^2 + (y - 2π)^2))
 θ_A = Array(θ)
 θ̅ .= 0.0
 
-t = [0.0]
-tend = 50.0 # 5 
 
-iend = ceil(Int, tend / Δt)
 
 simulation_parameters = (; ψ, A, 𝓀ˣ, 𝓀ʸ, x, y, φ, u, v, ∂ˣθ, ∂ʸθ, uθ, vθ, ∂ˣuθ, ∂ʸvθ, s, P, P⁻¹, filter)
-
 size_of_A = size(A)
+
+t = [0.0]
+tend = 50.0 # 50.0 is good for the default
+iend = ceil(Int, tend / Δt)
+global Δt_old = Δt
 
 realizations = 1000
 
 
 rhs! = θ_rhs_symmetric!
 
-T = 10.0
-nT = ceil(Int, T / Δt)
-Δt = T / nT
-iend = ceil(Int, tend / Δt)
+# T = 10.0
+for T in ProgressBar([10000.0, 25.0, 20.0, 15.0, 10.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.5, 0.4, 0.3, 0.2, 0.1])
+    nT = ceil(Int, T / Δt_old)
+    Δt = T / nT
+    iend = ceil(Int, tend / Δt)
 
-θ̅_timeseries = CuArray(zeros(size(ψ)..., iend))
-uθ_timeseries = CuArray(zeros(size(ψ)..., iend))
-θ_timeseries = Array(zeros(size(ψ)..., iend))
+    θ̅_timeseries = CuArray(zeros(size(ψ)..., iend))
+    uθ_timeseries = CuArray(zeros(size(ψ)..., iend))
+    θ_timeseries = Array(zeros(size(ψ)..., iend))
 
-ω = 2π / T  
-for j in ProgressBar(1:realizations)
-    # new realization of flow
-    rand!(rng, φ) # between 0, 1
-    φ .*= 2π # to make it a random phase
-    event = stream_function!(ψ, A, 𝓀ˣ, 𝓀ʸ, x, y, φ)
-    wait(event)
+    ω = 2π / T
+    for j in ProgressBar(1:realizations)
+        # new realization of flow
+        rand!(rng, φ) # between 0, 1
+        φ .*= 2π # to make it a random phase
+        event = stream_function!(ψ, A, 𝓀ˣ, 𝓀ʸ, x, y, φ)
+        wait(event)
 
-    t[1] = 0
-    @. s = sin(kˣ[2] * x) * cos(ω * t[1]) + 0 * kʸ
-
-    θ .= CuArray(θ_A)
-    for i = 1:iend
-        # fourth order runge kutta on deterministic part
-        # keep ψ frozen is the correct way to do it here
-
-        # the below assumes that φ is just a function of time
-        rhs!(k₁, θ, simulation_parameters)
-        @. θ̃ = θ + Δt * k₁ * 0.5
-        t[1] += Δt / 2
+        t[1] = 0
         @. s = sin(kˣ[2] * x) * cos(ω * t[1]) + 0 * kʸ
 
+        θ .= CuArray(θ_A)
+        for i = 1:iend
+            # fourth order runge kutta on deterministic part
+            # keep ψ frozen is the correct way to do it here
 
-        φ_rhs_normal!(φ̇, φ, rng)
-        @. φ += phase_speed * sqrt(Δt / 2) * φ̇
+            # the below assumes that φ is just a function of time
+            rhs!(k₁, θ, simulation_parameters)
+            @. θ̃ = θ + Δt * k₁ * 0.5
+            t[1] += Δt / 2
+            @. s = sin(kˣ[2] * x) * cos(ω * t[1]) + 0 * kʸ
 
-        rhs!(k₂, θ̃, simulation_parameters)
-        @. θ̃ = θ + Δt * k₂ * 0.5
-        rhs!(k₃, θ̃, simulation_parameters)
-        @. θ̃ = θ + Δt * k₃
-        t[1] += Δt / 2
-        @. s = sin(kˣ[2] * x) * cos(ω * t[1]) + 0 * kʸ
 
-        φ_rhs_normal!(φ̇, φ, rng)
-        @. φ += phase_speed * sqrt(Δt / 2) * φ̇
+            φ_rhs_normal!(φ̇, φ, rng)
+            @. φ += phase_speed * sqrt(Δt / 2) * φ̇
 
-        rhs!(k₄, θ̃, simulation_parameters)
-        @. θ += Δt / 6 * (k₁ + 2 * k₂ + 2 * k₃ + k₄)
+            rhs!(k₂, θ̃, simulation_parameters)
+            @. θ̃ = θ + Δt * k₂ * 0.5
+            rhs!(k₃, θ̃, simulation_parameters)
+            @. θ̃ = θ + Δt * k₃
+            t[1] += Δt / 2
+            @. s = sin(kˣ[2] * x) * cos(ω * t[1]) + 0 * kʸ
 
-        # update stochastic part 
-        # φ_rhs_normal!(φ̇, φ, rng)
-        # @. φ += sqrt(Δt) * φ̇
+            φ_rhs_normal!(φ̇, φ, rng)
+            @. φ += phase_speed * sqrt(Δt / 2) * φ̇
 
-        # save output
-        # tmp = real.(Array(θ))
-        P⁻¹ * uθ
-        @. θ̅_timeseries[:, :, i] += real.(θ) / realizations
-        @. uθ_timeseries[:, :, i] += real.(uθ) / realizations
-        if j == 1
-            θ_timeseries[:, :, i] = Array(real.(θ))
+            rhs!(k₄, θ̃, simulation_parameters)
+            @. θ += Δt / 6 * (k₁ + 2 * k₂ + 2 * k₃ + k₄)
+
+            # update stochastic part 
+            # φ_rhs_normal!(φ̇, φ, rng)
+            # @. φ += sqrt(Δt) * φ̇
+
+            # save output
+            # tmp = real.(Array(θ))
+            P⁻¹ * uθ
+            @. θ̅_timeseries[:, :, i] += real.(θ) / realizations
+            @. uθ_timeseries[:, :, i] += real.(uθ) / realizations
+            if j == 1
+                θ_timeseries[:, :, i] = Array(real.(θ))
+            end
+
         end
-        #=
-        if i % div(iend, 10) == 0
-            println("Saving at i=", i)
-            push!(ψ_save, Array(real.(ψ)))
-            push!(θ_save, Array(real.(θ)))
-            println("extrema are ", extrema(θ_save[end]))
-            println("time is t = ", t[1])
-        end
-
-        if t[1] >= tstart
-            θ̅ .+= Δt * θ
-        end
-
-        if i % div(iend, 100) == 0
-            println("time is t = ", t[1])
-            local toc = Base.time()
-            println("the time for the simulation is ", toc - tic, " seconds")
-            println("extrema are ", extrema(real.(θ)))
-            println("on wavenumber index ", index_choice)
-        end
-        =#
-
+        @. θ̅ += θ / realizations
     end
-    # println("finished realization ", j)
-    @. θ̅ += θ / realizations
+
+    toc = Base.time()
+    println("the time for the simulation was ", toc - tic, " seconds")
+
+    x_A = Array(x)[:] .- 2π
+    θ_F = Array(real.(θ))
+    θ̅_F = Array(real.(θ̅))
+
+    θ̅_timeseries_A = Array(θ̅_timeseries)
+    uθ_timeseries_A = Array(uθ_timeseries)
+    θ_timeseries_A = Array(θ_timeseries)
+
+    #=
+    begin
+        fig = Figure(resolution=(2048, 512))
+        ax11 = Axis(fig[1, 1])
+        ax12 = Axis(fig[2, 1])
+        ax13 = Axis(fig[3, 1])
+        t_slider = Slider(fig[4, 1:2], range=1:iend, startvalue=1)
+        tindex = t_slider.value
+
+        field1 = @lift(θ̅_timeseries_A[:, :, $tindex])
+        field2 = @lift(θ_timeseries_A[:, :, $tindex])
+        field3 = @lift(uθ_timeseries_A[:, :, $tindex])
+        upbound = maximum(abs.(θ̅_timeseries_A[:, :, end]))
+        hm1 = heatmap!(ax11, field1, colorrange=(-upbound, upbound), colormap=:balance)
+        hm2 = heatmap!(ax12, field2, colorrange=(-upbound, upbound), colormap=:balance)
+        hm3 = heatmap!(ax13, field3, colorrange=(-upbound, upbound), colormap=:balance)
+
+        Colorbar(fig[1, 2], hm1, height=Relative(3 / 4), width=25, ticklabelsize=30, labelsize=30, ticksize=25, tickalign=1,)
+        Colorbar(fig[2, 2], hm2, height=Relative(3 / 4), width=25, ticklabelsize=30, labelsize=30, ticksize=25, tickalign=1,)
+        Colorbar(fig[3, 2], hm3, height=Relative(3 / 4), width=25, ticklabelsize=30, labelsize=30, ticksize=25, tickalign=1,)
+        display(fig)
+    end
+    =#
+    begin
+        fig2 = Figure(resolution=(1400, 600))
+        ax11 = Axis(fig2[1, 1]; title="⟨θ⟩: averaged over y", xlabel="spatial index", ylabel="time index")
+        ax21 = Axis(fig2[2, 1]; title="⟨θ⟩: black = index 32 of above, red = scaled forcing", xlabel="time index", ylabel="value")
+        ax12 = Axis(fig2[1, 2]; title="⟨uθ⟩: averaged over y", xlabel="spatial index", ylabel="time index")
+        ax22 = Axis(fig2[2, 2]; title="⟨uθ⟩: black = index 64 of above, red = scaled forcing", xlabel="time index", ylabel="value")
+
+        mtheta2 = mean(θ̅_timeseries_A, dims=2)[:, 1, :]
+        mutheta2 = mean(uθ_timeseries_A, dims=2)[:, 1, :]
+        mtheta2max = maximum(mtheta2)
+        mutheta2max = maximum(mutheta2)
+        heatmap!(ax11, mtheta2, colorrange=(-mtheta2max, mtheta2max), colormap=:balance)
+        heatmap!(ax12, mutheta2, colorrange=(-mutheta2max, mutheta2max), colormap=:balance)
+        lines!(ax21, mtheta2[32, :], color=:black, linewidth=2)
+        amp = maximum(mtheta2[32, :])
+        lines!(ax21, amp .* cos.(ω .* collect(1:iend) * Δt), color=:red, linewidth=2)
+
+        lines!(ax22, mutheta2[64, :], color=:black, linewidth=2)
+        amp = maximum(mutheta2[64, :])
+        lines!(ax22, amp .* cos.(ω .* collect(1:iend) * Δt), color=:red, linewidth=2)
+        save("time_dependentSummary_plot_ω_" * string(ω)  * "_ensemble_" * string(realizations) * ".png", fig2)
+        using HDF5
+        fid = h5open("time_dependent_ω_" * string(ω) * "_ensemble_" * string(realizations) * ".hdf5", "w")
+        fid["molecular_diffusivity"] = κ
+        fid["streamfunction_amplitude"] = Array(A)
+        fid["phase increase"] = phase_speed
+        fid["time"] = collect(Δt * (1:iend))
+        fid["omega"] = ω
+        fid["ensemble mean"] = mtheta2
+        fid["ensemble flux"] = mutheta2
+        fid["ensemble number"] = realizations
+        close(fid)
+    end
 end
-
-toc = Base.time()
-println("the time for the simulation was ", toc - tic, " seconds")
-
-x_A = Array(x)[:] .- 2π
-θ_F = Array(real.(θ))
-θ̅_F = Array(real.(θ̅))
-
-θ̅_timeseries_A = Array(θ̅_timeseries)
-uθ_timeseries_A = Array(uθ_timeseries)
-θ_timeseries_A = Array(θ_timeseries)
-
-begin
-    fig = Figure(resolution=(2048, 512))
-    ax11 = Axis(fig[1, 1])
-    ax12 = Axis(fig[2, 1])
-    ax13 = Axis(fig[3, 1])
-    t_slider = Slider(fig[4, 1:2], range=1:iend, startvalue=1)
-    tindex = t_slider.value
-
-    field1 = @lift(θ̅_timeseries_A[:, :, $tindex])
-    field2 = @lift(θ_timeseries_A[:, :, $tindex])
-    field3 = @lift(uθ_timeseries_A[:, :, $tindex])
-    upbound = maximum(abs.(θ̅_timeseries_A[:, :, end]))
-    hm1 = heatmap!(ax11, field1, colorrange=(-upbound, upbound), colormap=:balance)
-    hm2 = heatmap!(ax12, field2, colorrange=(-upbound, upbound), colormap=:balance)
-    hm3 = heatmap!(ax13, field3, colorrange=(-upbound, upbound), colormap=:balance)
-
-    Colorbar(fig[1, 2], hm1, height=Relative(3 / 4), width=25, ticklabelsize=30, labelsize=30, ticksize=25, tickalign=1,)
-    Colorbar(fig[2, 2], hm2, height=Relative(3 / 4), width=25, ticklabelsize=30, labelsize=30, ticksize=25, tickalign=1,)
-    Colorbar(fig[3, 2], hm3, height=Relative(3 / 4), width=25, ticklabelsize=30, labelsize=30, ticksize=25, tickalign=1,)
-    display(fig)
-end
-
-begin
-    fig2 = Figure(resolution=(1400, 600))
-    ax11 = Axis(fig2[1, 1]; title = "⟨θ⟩: averaged over y", xlabel = "spatial index", ylabel = "time index")
-    ax21 = Axis(fig2[2, 1]; title = "⟨θ⟩: black = index 32 of above, red = scaled forcing", xlabel = "time index", ylabel = "value")
-    ax12 = Axis(fig2[1, 2]; title = "⟨uθ⟩: averaged over y", xlabel = "spatial index", ylabel = "time index")
-    ax22 = Axis(fig2[2, 2]; title = "⟨uθ⟩: black = index 64 of above, red = scaled forcing", xlabel = "time index", ylabel = "value")
-
-    mtheta2 = mean(θ̅_timeseries_A, dims=2)[:, 1, :]
-    mutheta2 = mean(uθ_timeseries_A, dims=2)[:, 1, :]
-    mtheta2max = maximum(mtheta2)
-    mutheta2max = maximum(mutheta2)
-    heatmap!(ax11, mtheta2, colorrange=(-mtheta2max, mtheta2max), colormap=:balance)
-    heatmap!(ax12, mutheta2, colorrange=(-mutheta2max, mutheta2max), colormap=:balance)
-    lines!(ax21, mtheta2[32, :], color=:black, linewidth=2)
-    amp = maximum(mtheta2[32, :])
-    lines!(ax21, amp .* cos.(ω .* collect(1:iend) * Δt), color=:red, linewidth=2)
-
-    lines!(ax22, mutheta2[64, :], color=:black, linewidth=2)
-    amp = maximum(mutheta2[64, :])
-    lines!(ax22, amp .* cos.(ω .* collect(1:iend) * Δt), color=:red, linewidth=2)
-    save("TimeDependentSummaryPlot_ω_" * string(ω) * ".png", fig2)
-end
-
 ##
 #=
 begin

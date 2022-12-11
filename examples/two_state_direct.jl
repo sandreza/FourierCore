0192356b66d4e32cfe91af1ebaae51c9c34e246d
sandreza <andrenogueirasouza@gmail.com>
2022-12-11 18:19:40 -0500

    direct calculation

--- examples/two_state_direct.jl
+++ examples/two_state_direct.jl
@@ -10,13 +10,16 @@ include("random_phase_kernel.jl")
 include("markov_chain_hammer_imports.jl")
 using CUDA
 arraytype = CuArray
-Ω = S¹(4π)^2
+Ω = S¹(2π)^2
 N = 2^7 # number of gridpoints
 M = 2 # number of states
-U = 1 # amplitdue factor
+U = 1.0 # amplitude factor
 Q = ou_transition_matrix(M - 1)
-A = collect(range(-sqrt(M - 1), sqrt(M - 1), length=M))
+A = U .* collect(range(-sqrt(M - 1), sqrt(M - 1), length=M))
 p = steady_state(Q)
+Λ, V = eigen(Q)
+V[:, end] .= p
+V⁻¹ = inv(V)
 
 grid = FourierGrid(N, Ω, arraytype=arraytype)
 nodes, wavenumbers = grid.nodes, grid.wavenumbers
@@ -43,11 +46,11 @@ v = similar(ψ)
 θ̇s = [similar(ψ) .* 0 for i in 1:M]
 s = similar(ψ)
 θ̅ = similar(ψ)
-k₁ = similar(ψ)
-k₂ = similar(ψ)
-k₃ = similar(ψ)
-k₄ = similar(ψ)
-θ̃ = similar(ψ)
+k₁ = [similar(ψ) for i in 1:M]
+k₂ = [similar(ψ) for i in 1:M]
+k₃ = [similar(ψ) for i in 1:M]
+k₄ = [similar(ψ) for i in 1:M]
+θ̃ = [similar(ψ) for i in 1:M]
 uθ = similar(ψ)
 vθ = similar(ψ)
 ∂ˣuθ = similar(ψ)
@@ -55,31 +58,41 @@ vθ = similar(ψ)
 
 # source
 s = similar(ψ)
-@. s = cos(kˣ[2] * x) * cos(kʸ[2] * y) # could also set source term to zero
+@. s = sin(kˣ[4] * x) * sin(kʸ[4] * y) # could also set source term to zero
 
 # operators
 ∂x = im * kˣ
 ∂y = im * kʸ
 Δ = @. ∂x^2 + ∂y^2
+κ = 0.01
+
+# set equal to diffusive solution 
+tmp = (kˣ[4]^2 + kʸ[4]^2)
+for (i, θ) in enumerate(θs)
+    pⁱ = p[i]
+    θ .= (s ./ (tmp * κ)) .* pⁱ
+end
 
 # plan ffts
 P = plan_fft!(ψ)
 P⁻¹ = plan_ifft!(ψ)
 
-##
-Δx = x[2] - x[1]
-κ = 0.01
-cfl = 0.1
-Δx = (x[2] - x[1])
-advective_Δt = cfl * Δx / U
-diffusive_Δt = cfl * Δx^2 / κ
-
 # set stream function and hence velocity
 @. ψ = cos(kˣ[2] * x) * cos(kʸ[2] * y)
 P * ψ # in place fft
 # ∇ᵖψ
-@. u = -1.0 * (∂y * ψ); @. v = (∂x * ψ);
-P⁻¹ * ψ; P⁻¹ * u; P⁻¹ * v; # don't need ψ anymore
+@. u = -1.0 * (∂y * ψ);
+@. v = (∂x * ψ);
+P⁻¹ * ψ;
+P⁻¹ * u;
+P⁻¹ * v; # don't need ψ anymore
+
+## timestepping
+Δx = x[2] - x[1]
+cfl = 0.1 #0.1
+advective_Δt = cfl * Δx / maximum(real.(u))
+diffusive_Δt = cfl * Δx^2 / κ
+Δt = min(advective_Δt, diffusive_Δt)
 
 simulation_parameters = (; A, p, Q, u, v, ∂ˣθ, ∂ʸθ, uθ, vθ, ∂ˣuθ, ∂ʸvθ, s, P, P⁻¹, ∂x, ∂y, κ, Δ, κΔθ)
 
@@ -88,20 +101,27 @@ function n_state_rhs_symmetric!(θ̇s, θs, simulation_parameters)
 
     # need A (amplitude), p (probability of being in state), Q (transition probability)
     for (i, θ) in enumerate(θs)
-        Aⁱ = A[i]; pⁱ = p[i]; θ̇ = θ̇s[i]
+        Aⁱ = A[i]
+        pⁱ = p[i]
+        θ̇ = θ̇s[i]
         # dynamics
         P * θ # in place fft
         # ∇θ
-        @. ∂ˣθ = ∂x * θ; @. ∂ʸθ = ∂y * θ
+        @. ∂ˣθ = ∂x * θ
+        @. ∂ʸθ = ∂y * θ
         # κΔθ
         @. κΔθ = κ * Δ * θ
         # go back to real space 
         [P⁻¹ * field for field in (θ, ∂ˣθ, ∂ʸθ, κΔθ)] # in place ifft
         # compute u * θ and v * θ take derivative and come back
-        @. uθ = u * θ; @. vθ = v * θ
-        P * uθ; P * vθ
-        @. ∂ˣuθ = ∂x * uθ; @. ∂ʸvθ = ∂y * vθ
-        P⁻¹ * ∂ˣuθ; P⁻¹ * ∂ʸvθ
+        @. uθ = u * θ
+        @. vθ = v * θ
+        P * uθ
+        P * vθ
+        @. ∂ˣuθ = ∂x * uθ
+        @. ∂ʸvθ = ∂y * vθ
+        P⁻¹ * ∂ˣuθ
+        P⁻¹ * ∂ʸvθ
         # compute θ̇ in real space
         @. θ̇ = -Aⁱ * (u * ∂ˣθ + v * ∂ʸθ + ∂ˣuθ + ∂ʸvθ) * 0.5 + κΔθ + s * pⁱ
         # transitions
@@ -115,3 +135,43 @@ function n_state_rhs_symmetric!(θ̇s, θs, simulation_parameters)
 end
 
 n_state_rhs_symmetric!(θ̇s, θs, simulation_parameters)
+
+rhs! = n_state_rhs_symmetric!
+
+tend = 10.0
+iend = ceil(Int, tend / Δt)
+
+# runge kutta 4 timestepping
+for i in ProgressBar(1:iend)
+    rhs!(k₁, θs, simulation_parameters)
+    [θ̃[i] .= θs[i] .+ Δt * k₁[i] * 0.5 for i in eachindex(θs)]
+    rhs!(k₂, θ̃, simulation_parameters)
+    [θ̃[i] .= θs[i] .+ Δt * k₂[i] * 0.5 for i in eachindex(θs)]
+    rhs!(k₃, θ̃, simulation_parameters)
+    [θ̃[i] .= θs[i] .+ Δt * k₃[i] * 0.5 for i in eachindex(θs)]
+    rhs!(k₄, θ̃, simulation_parameters)
+    [θs[i] += Δt / 6 * (k₁[i] + 2 * k₂[i] + 2 * k₃[i] + k₄[i]) for i in eachindex(θs)]
+end
+
+println("maximum value of theta ", maximum(real.(θs[1] + θs[2])))
+
+# plot
+using GLMakie
+xA = Array(x)[:]
+yA = Array(y)[:]
+fig = Figure()
+colorrange = (-1, 1)
+for i in eachindex(θs)
+    ax = Axis(fig[1, i];  title="state $i")
+    θA = real.(Array(θs[i]))
+    contourf!(ax, xA, yA, θA, colormap=:balance)
+
+    θA .= 0.0
+    ax2 = Axis(fig[2, i]; title= "spectral state $i")
+    for j in eachindex(θs)
+        θA .+= real.(Array(θs[j])) .* V⁻¹[end - i + 1,j]
+    end
+    contourf!(ax2, xA, yA, θA, colormap=:balance)
+end
+
+display(fig)

aeb782ec5adb19f7fede6cfef9a3a7d11401effa
sandreza <andrenogueirasouza@gmail.com>
2023-09-26 11:47:16 -0400

    full kernel

--- current/full_kernel.jl
+++ current/full_kernel.jl
@@ -315,7 +315,8 @@ kernel3 = -real.(circshift(kernel2, (41, 0)))
 
 ##
 Tfundamental = 2π / ωs[2]
-times = Tfundamental / (size(kernel3)[2]-1) * collect(0:4)
+plotting_indices = collect(1:50)
+times = Tfundamental / (size(kernel3)[2]-1) * (plotting_indices .- 1)
 nK = 2*kindices + 1
 xs = collect(0:nK-1) / nK .* 2π
 fig = Figure(resolution = (1400, 800))
@@ -323,8 +324,8 @@ ax11 = Axis(fig[1,1]; title = "kernel for different time lags")
 op = 0.5
 colorlist = [(:red, op), (:blue, op), (:green, op), (:orange, op), (:purple, op)]
 lw = 3
-for i in eachindex(times)
-    lines!(ax11, xs, kernel3[:, i], label = string("t = ") * string(times[i]), color = colorlist[i], linewidth = lw)
+for (j, i) in enumerate([1, 8+1, 16+1, 24+1, 32+1])
+    lines!(ax11, xs, kernel3[:, plotting_indices[i]], label = string("t = ") * string(times[i]), color = colorlist[j], linewidth = lw)
 end
 axislegend(ax11, position=:rt, framecolor=(:grey, 0.5), patchsize=(30, 30), markersize=50, labelsize=20)
 
@@ -335,6 +336,7 @@ ax21 = Axis(fig[2,1]; title = "space time kernel", ylabel = "time", xlabel = "sp
 ts = Tfundamental / (size(kernel3)[2]-1) * collect(0:20)
 heatmap!(ax21, xs, ts, kernel3[:, 1:21])
 
+ts = Tfundamental / (size(kernel3)[2]-1) * collect(0:100)
 ax22 = Axis(fig[2,2]; title = "kernel peak as a function of time", ylabel = "peak", xlabel = "time")
 scatter!(ax22, ts, [maximum(kernel3[:, i]) for i in eachindex(ts)], label = "peak")
 display(fig)

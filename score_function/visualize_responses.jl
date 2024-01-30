988b119a1588e9bbdea88c16b2478c235cd62982
Andre Souza <andrenogueirasouza@gmail.com>
2024-01-30 16:11:41 -0500

    update visualize response

--- score_function/visualize_responses.jl
+++ score_function/visualize_responses.jl
@@ -18,7 +18,7 @@ for i in 1:4
     ax = Axis(fig[ii, jj]; title = "1 -> " * string(indexchoice), xlabel = "time", ylabel = "response")
     lines!(ax, ts, a_linear_response[i, 1, :], color = (:blue, 0.4), linewidth = lw, label = "linear")
     lines!(ax, ts, a_hack_score_response[i, 1, :], color = (:red, 0.4), linewidth = lw, label = "score")
-    lines!(ax, ts, a_generative_response[i, 1, :], color = (:green, 0.4), linewidth = lw, label = "generative")
+    # lines!(ax, ts, a_generative_response[i, 1, :], color = (:green, 0.4), linewidth = lw, label = "generative")
     scatter!(ax,ts, a_numerical_response[i, 1, :], color = (:orange, 0.4), linewidth = lw, label = "perturbation")
     if i == 1
         axislegend(ax, position = :rt)
@@ -29,8 +29,7 @@ for i in 1:4
     ylims!(ax, (-0.15, 1.1))
 end
 display(fig)
-save("responses.png", fig)
-
+save("nonlinear_responses.png", fig)
 
 ##
 hfile = h5open("data_0.0_0.0_0.0_0.0_responses.hdf5", "r")
@@ -42,7 +41,33 @@ hfile = h5open("data_0.0_0.0_0.0_0.0_generative_response.hdf5", "r")
 l_a_generative_response = read(hfile["generative response"])
 close(hfile)
 
-fig = Figure(resolution = (777, 233))
+fig2 = Figure(resolution = (777, 233))
+N = 32
+lw = 3
+ts = collect(0:99)
+for i in 1:4
+    indexchoice = i
+    ii = (i-1)÷4 + 1
+    jj = (i-1)%4 + 1
+    ax = Axis(fig2[ii, jj]; title = "1 -> " * string(indexchoice), xlabel = "time", ylabel = "response")
+    lines!(ax, ts, l_a_linear_response[i, 1, :], color = (:blue, 0.4), linewidth = lw, label = "linear")
+    lines!(ax, ts, l_a_hack_score_response[i, 1, :], color = (:red, 0.4), linewidth = lw, label = "score")
+    lines!(ax, ts, l_a_generative_response[i, 1, 1:100], color = (:green, 0.4), linewidth = lw, label = "generative")
+    scatter!(ax,ts, l_a_numerical_response[i, 1, :], color = (:orange, 0.4), linewidth = lw, label = "perturbation")
+    if i == 1
+        axislegend(ax, position = :rt)
+    else
+        hideydecorations!(ax)
+    end
+    xlims!(ax, (0, 50))
+    ylims!(ax, (-0.15, 1.1))
+end
+display(fig2)
+save("linear_responses.png", fig2)
+
+##
+
+fig = Figure(resolution = (2*777, 2*233))
 N = 32
 lw = 3
 ts = collect(0:99)
@@ -51,6 +76,23 @@ for i in 1:4
     ii = (i-1)÷4 + 1
     jj = (i-1)%4 + 1
     ax = Axis(fig[ii, jj]; title = "1 -> " * string(indexchoice), xlabel = "time", ylabel = "response")
+    lines!(ax, ts, a_linear_response[i, 1, :], color = (:blue, 0.4), linewidth = lw, label = "linear")
+    lines!(ax, ts, a_hack_score_response[i, 1, :], color = (:red, 0.4), linewidth = lw, label = "score")
+    lines!(ax, ts, a_generative_response[i, 1, :], color = (:green, 0.4), linewidth = lw, label = "generative")
+    scatter!(ax,ts, a_numerical_response[i, 1, :], color = (:orange, 0.4), linewidth = lw, label = "perturbation")
+    if i == 1
+        axislegend(ax, position = :rt)
+    else
+        hideydecorations!(ax)
+    end
+    xlims!(ax, (0, 50))
+    ylims!(ax, (-0.15, 1.1))
+end
+
+ts = collect(0:99)
+for i in 1:4
+    indexchoice = i
+    ax = Axis(fig[2, i]; title = "1 -> " * string(indexchoice), xlabel = "time", ylabel = "response")
     lines!(ax, ts, l_a_linear_response[i, 1, :], color = (:blue, 0.4), linewidth = lw, label = "linear")
     lines!(ax, ts, l_a_hack_score_response[i, 1, :], color = (:red, 0.4), linewidth = lw, label = "score")
     lines!(ax, ts, l_a_generative_response[i, 1, 1:100], color = (:green, 0.4), linewidth = lw, label = "generative")
@@ -64,4 +106,5 @@ for i in 1:4
     ylims!(ax, (-0.15, 1.1))
 end
 display(fig)
-save("linear_responses.png", fig)
+##
+save("together_responses.png", fig)
